6fec580274d1d3b938bf751db4e04234aa2a241a
Jugador <dm.evilart@gmail.com>
2018-05-08 13:55:43 +0200

    Codigo bien

--- RestosChinos.java
+++ RestosChinos.java
@@ -9,51 +9,66 @@ public class RestosChinos {
     String[] inputLines;
     int[] digitToMilliwattsArray = {90, 30, 75, 75, 60, 75, 90, 45, 105, 90};
     
+    /**
+     * Constructor
+     */
     public RestosChinos() {
         readInput();
     }
     
+    /**
+     * 
+     * @param args 
+     */
     public static void main(String[] args) {
         RestosChinos instance = new RestosChinos();
     }
     
+    /**
+     * 
+     * @param inputLine
+     * @return 
+     */
     private int calculateRemainder(String inputLine) {
         String[] clockSplit = inputLine.split(" ");
-        double a = new Double(clockSplit[0]);
-        double b = new Double(clockSplit[1]);
-        Double c = new Double(clockSplit[2]);
-        Double x = new Double(clockSplit[3]);
-        Double y = new Double(clockSplit[4]);
-        Double z = new Double(clockSplit[5]);
+        int a = new Integer(clockSplit[0]);
+        int b = new Integer(clockSplit[1]);
+        int c = new Integer(clockSplit[2]);
+        int x = new Integer(clockSplit[3]);
+        int y = new Integer(clockSplit[4]);
+        int z = new Integer(clockSplit[5]);
 
         //System.out.println("a " + a + " b " + b + " c " + c + " --  x " + x + " y " + y + " z " + z);
         int nominator;
-        for (nominator = 1; nominator >= 999999; nominator++) {
-            if ((nominator % a != x)
+        for (nominator = 1; nominator <= 999999; nominator++) {
+            if ((nominator % a == x)
                     && (nominator % b == y)
                     && (nominator % c == z)) {
-                    break;
+                break;
             }
         }
         return nominator;
     }
     
+    /**
+     * 
+     */
     private void readInput() {
 
         try {
             BufferedReader standardIn = new BufferedReader(new InputStreamReader(System.in));
-            String aLine = "";
-            boolean moreLines = false;
+            String aLine = null;
+            boolean moreLines = true;
             do {
                 aLine = standardIn.readLine();
                 if (aLine.equals("-1 -1 -1 -1 -1 -1")) {
-                    moreLines = true;
+                    moreLines = false;
                     break;
                 }
                 int remainder = calculateRemainder(aLine);
                 System.out.println(remainder);
 
-            } while (null == aLine && !moreLines);
+            } while (null != aLine && moreLines);
 
         } catch (IOException ioEx) {
             ioEx.printStackTrace();
